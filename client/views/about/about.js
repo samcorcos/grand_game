40d7225bfe6ece8fe72f580b31bdb2de7d60b964
Sam Corcos <sam@andaril.com>
2014-11-15 13:25:57 -0500

    added meteor-kitchen minimalist template

---
+++ client/views/about/about.js
@@ -0,0 +1,14 @@
+Template.About.rendered = function() {
+	
+};
+
+Template.About.events({
+	"click #page-close-button": function(e, t) {
+		e.preventDefault();
+		Router.go("", {});
+	}
+});
+
+Template.About.helpers({
+
+});
