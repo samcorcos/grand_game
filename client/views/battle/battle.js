7aeda45e559a2c90a71053c4f1258bdbc94337e2
Sam Corcos <sam@andaril.com>
2014-11-16 20:56:23 -0500

    changed the combat form into something that makes more sense

--- client/views/battle/battle.js
+++ client/views/battle/battle.js
@@ -18,7 +18,7 @@
 // This needs to be "submit", not "click", but I can't get "submit" to work properly
 // The timer is really important!!! Otherwise, people won't trust the computer.
 Template.battle.events({
-  "click #submit-combat-button": function (event, template) {
+  "submit #submit-combat-button": function (event, template) {
     Meteor.setTimeout(function() {
       Router.go("/battle/outcome", {});
     }, 1000)
@@ -27,7 +27,6 @@ Template.battle.events({
 
 Template.battle.events({
   "click #add-combatant-button": function (event, template) {
-
     UI.insert(UI.render(Template.combat), $("#combatant-list").get(0));
     // $("#combatant-list").after("{{> combat}}");
   }
