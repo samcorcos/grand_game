4af30ffd4b96ff671cddb2e90ad3b3885545b4a6
Sam Corcos <sam@andaril.com>
2014-11-17 10:47:42 -0500

    adding helpers to outcome

--- client/views/battle_outcome/outcome.js
+++ client/views/battle_outcome/outcome.js
@@ -1,1 +1,21 @@
 //going to need helpers to give name of each person
+
+Template.winner.helpers({
+  winnerName: "King George"
+});
+
+Template.combatant.helpers({
+  combatantName: "Mike"
+});
+
+Template.armor.helpers({
+  armorLosses: 5
+});
+
+Template.infantry.helpers({
+  infantryLosses: 2
+});
+
+Template.airplane.helpers({
+  airplaneLosses: 1
+});
