f98940c80305b55443e9c5466a8f043fb7a6205b
Sam Corcos <samcorcos@gmail.com>
2014-11-29 12:33:34 -0800

    turned the battles page into cards

--- client/views/politics/politics.js
+++ client/views/politics/politics.js
@@ -1,5 +1,12 @@
+Politics = new Mongo.Collection(null);
+
 Template.politics.events({
   "click #submit-politics-button": function (event, template) {
-      Router.go("/politics/outcome", {});
+      event.preventDefault();
+      Router.go("/politics/outcome", {test: "it passed"});
   }
 });
+
+// Template.politics.events({
+//   ""
+// })
